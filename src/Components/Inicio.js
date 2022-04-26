6c6e917e45579fc6592119b1cd22af2f748711a4
DiogoSales07 <dsales289@poli.ufrj.br>
2022-04-26 11:29:23 -0300

    Finalizando o nivel 1

--- src/Components/Inicio.js
+++ src/Components/Inicio.js
@@ -10,9 +10,10 @@ import hojeEuQueroVoltarSozinho from "../Assets/HojeEuQueroVoltarSozinho.png"
 import capitaoFantastico from "../Assets/CapitaoFantastico.png"
 import fugaDasGalinhas from "../Assets/FugaDasGalinhas.png"
 import shrekPoster from "../Assets/Shrek.png"
-import like from "../Assets/Like.png"
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import favorite from "../Assets/favorite.png"
+import {faThumbsUp, faSearch} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Main = styled.div`
 margin-left:4vw;
@@ -46,8 +47,7 @@ display:flex;
 flex-direction:column;
 flex-wrap:wrap;
 align-items:flex-start;
-width:15.9rem;
-border:solid black;
+width:16rem;
 height:15rem;
 margin-top:0.5rem;
 
@@ -57,7 +57,7 @@ margin-top:0.5rem;
 
 const Poster = styled.img`
 border-radius:5px;
-width:15.5rem;
+width:15.7rem;
 height:8.3rem;`
 
 const Overview = styled.li`
@@ -69,7 +69,8 @@ padding-top:0.1rem;
 font-size:Medium;
 font-weight:bold;
 display:flex;
-justify-content:space-between;`
+justify-content:space-between;
+flex-wrap:wrap;`
 
 const LikeBtn = styled.button`
 border-radius:50%;
@@ -96,6 +97,26 @@ width:27vw;
 left:66.5%;
 bottom:93%;`
 
+const Ul = styled.ul`
+position:relative;`
+
+const ListFav = styled.li`
+position:absolute;
+left:89%;
+top:2%;`
+
+const RatingSection = styled.div`
+width:3.5rem;
+display:flex;
+justify-content:space-evenly;
+align-items:flex-start;
+`
+
+const Principal = styled.div`
+width:11.5rem;
+display:flex;
+flex-wrap:wrap;
+align-items:center;`
 
 export default class Inicio extends React.Component{
   state={
@@ -104,31 +125,37 @@ export default class Inicio extends React.Component{
         name:'Shrek',
         image: <Poster src={shrekPoster} alt ="poster do filme: sherek"/>,
         overview:'Para resgatar uma princesa das garras de um dragão que cospe fogo, o ogro Shrek se une a um companheiro improvável: um burro brincalhão.',
+        rating:'4/5'
       },
       {
         name:'Hoje Eu Quero Voltar sozinho',
         image:<Poster src={hojeEuQueroVoltarSozinho} alt ="poster do filme: Hoje Eu Quero Voltar Sozinho"/>,
         overview:'Um novo colega de sala transforma a vida do estudante Leonardo, que também é cego, e complica sua amizade com sua melhor amiga.',
+        rating:'-'
       },
       {
         name:'Spider-man',
         image:<Poster src={spiderMan} alt ="poster do filme: Spider-Man no multiverso"/>,
         overview:'Após ser picado por uma aranha radioativa, o garoto Miles Morales logo aprende a lançar teias com seus parceiros de um universo alternativo.',
+        rating:'3/5'
       },
       {
         name:'Um Sonho de Liberdade',
         image:<Poster src={sonhoDeLiberdade} alt ="poster do filme: Sonho de Liberdade"/>,
         overview:'Condenado pelo assassinato da esposa e do amante dela, um banqueiro se apega à esperança e à amizade com um detento chamado Red para sobreviver à prisão.',
+        rating:'4,5/5'
       },
       {
         name:'Que Horas Ela Volta?',
         image:<Poster src={queHorasElaVolta} alt ="poster do filme: Que Horas Ela Volta?"/>,
         overview:'Val é a fiel empregada domestica de uma família rica. Mas a chegada de sua filha gera tensão na casa e faz com que ela comece a questionar esse papel.',
+        rating:'2/5'
       },
       {
         name:'A Fuga das Galinhas',
         image:<Poster src={fugaDasGalinhas} alt="poster do filme: A Fuga das Galinhas"/>,
         overview:'O galo Rocky e a galinha Ginger querem ajudar todas as outras galinhas a fugirem da granja onde vivem em cativeiro.',
+        rating:'5/5'
       },
     ],
     destaque:[
@@ -140,12 +167,14 @@ export default class Inicio extends React.Component{
     ],
     myMovies:[],
     spotlightMovie:[],
+    favoriteMovies:[],
   }
 
   async componentDidMount(){
     const firstMovies = this.state.movies.map((item) => {
       return {
-        ...item
+        ...item,
+        id: Date.now(),
       };
     });
     this.setState({
@@ -180,6 +209,7 @@ export default class Inicio extends React.Component{
         myMovies: finalMovie
     })
 }
+
     render(){
         return(
           <Main>
@@ -192,13 +222,14 @@ export default class Inicio extends React.Component{
             <Spotlight>
               <div>
                 {item.image}
+                <button></button>
               </div>
               <PrincipalInfos>
-                <li><LikeBtn><img src={like} alt = "botão de filmes favoritos" /></LikeBtn></li>
+                <li><LikeBtn><img src={favorite} alt = "botão de filmes favoritos" /></LikeBtn></li>
                 <h4>Visto recentemente</h4>
                 <li><h1>{item.name}</h1></li>
                 <Overview>{item.overview}</Overview>
-                <li>rating</li>
+                <li>Rating</li>
               </PrincipalInfos>
             </Spotlight>
           ))}
@@ -219,14 +250,20 @@ export default class Inicio extends React.Component{
             >
             {this.state.myMovies.map((item) => (
               <Exibtion>
-                <ul>
+                <Ul>
+                  <ListFav><LikeBtn><img src={favorite} alt = "botão de filmes favoritos" /></LikeBtn></ListFav>
                   <li>{item.image}</li>
                   <Title>
-                    <p>{item.name}</p>
-                    <p>rating</p>
+                    <Principal>
+                      <p>{item.name}</p>
+                    </Principal>
+                    <RatingSection>
+                      <h6>{item.rating}</h6>
+                      <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
+                    </RatingSection>
                   </Title>
                   <Overview>{item.overview}</Overview>
-                </ul>
+                </Ul>
               </Exibtion>
                 ))}
             </Carousel>
